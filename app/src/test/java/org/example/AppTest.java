ff36eedfa782a8289d5bbd49d6d1840ff278371e
mosunou <yoshida@github.com>
2025-06-30 10:44:23 +0900

    return AppTest

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -9,6 +9,6 @@ import static org.junit.Assert.*;
 public class AppTest {
     @Test public void testAppReternHelloWorld() {
         App classUnderTest = new App();
-        assertEquals("Hello World", classUnderTest.getGreeting());
+        assertEquals("Hello World!", classUnderTest.getGreeting());
   }
 }
